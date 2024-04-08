1ed035646c647dd47fd0992c4270f99635af6dfd
garlicrybread <garlicrybread@gmail.com>
2024-04-08 11:23:07 -0400

    record button starts speech recognition microphone

--- flaskr/static/js/myJS.js
+++ flaskr/static/js/myJS.js
@@ -4,14 +4,15 @@ let audioURLs = []; // Array to store multiple audio URLs
 let recordingIndex = 0;
 let stateIndex = 0;
 let stream;
-// let practicePath;
-// if (window.location.href.includes("/pronunciationVis/")) {
-//     practicePath = "/pronunciationVis/practice"; // Set for remote
-// } else {
-//     practicePath = "/practice"; // Set for local
-// }
+let practicePath;
+if (window.location.href.includes("/pronunciationVis/")) {
+    practicePath = "/pronunciationVis/practice"; // Set for remote
+} else {
+    practicePath = "/practice"; // Set for local
+}
 
 function toggleText(buttonId) {
+    console.log('toggling button')
     var button = document.getElementById(buttonId);
     var loader_number;
     if (buttonId == "myButton") {
@@ -39,57 +40,60 @@ function toggleText(buttonId) {
     }
 }
 
-// function startRecording() {
-//    // Prepare the data to be sent to the server
-//     var requestData = {
-//         word: 'YourWordHere', // Provide the word you want to record
-//         participantID: 'test123', // Provide the participant ID
-//         debug: false // Set debug mode if needed
-//     };
-//
-//     // Make a fetch request to your server endpoint where the Python function is executed
-//     // const path = window.location.href
-//     fetch(`${practicePath}/api/record`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(requestData)
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//         }
-//         return response.text();
-//     })
-//     .then(data => {
-//         // Handle success
-//         console.log('File recorded successfully:', data);
-//     })
-//     .catch(error => {
-//         // Handle errors
-//         console.error('Error recording file:', error);
-//     });
-// }
-
-
 function startRecording() {
-    navigator.mediaDevices.getUserMedia({
-        audio: true
-    }).then(stream => {
-        mediaRecorders[recordingIndex] = new MediaRecorder(stream);
+    // Prepare the data to be sent to the server
+    // todo: request data should not be hardcoded
+    var requestData = {
+        word: 'YourWordHere', // Provide the word you want to record
+        participantID: 'test', // Provide the participant ID
+        debug: false // Set debug mode if needed
+    };
 
-        mediaRecorders[recordingIndex].ondataavailable = (e) => {
-            chunks.push(e.data);
-        };
-
-        mediaRecorders[recordingIndex].start();
-
-        stateIndex = 1; //meaning that i have started recording.
-    }).catch(error => {
-        console.log('Following error has occurred: ', error);
+    // Make a fetch request to your server endpoint where the Python function is executed
+    // const path = window.location.href
+    fetch(`${practicePath}/api/record`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        // Handle success
+        console.log('File recorded successfully:', data);
+        // TODO: variable button id
+        toggleText('myButton')
+    })
+    .catch(error => {
+        // Handle errors
+        console.error('Error recording file:', error);
     });
 }
+
+
+// function startRecording() {
+//     navigator.mediaDevices.getUserMedia({
+//         audio: true
+//     }).then(stream => {
+//         mediaRecorders[recordingIndex] = new MediaRecorder(stream);
+//
+//         mediaRecorders[recordingIndex].ondataavailable = (e) => {
+//             chunks.push(e.data);
+//         };
+//
+//         mediaRecorders[recordingIndex].start();
+//
+//         stateIndex = 1; //meaning that i have started recording.
+//     }).catch(error => {
+//         console.log('Following error has occurred: ', error);
+//     });
+// }
 function startAnimation(loaderNumber) {
     var animationElement = document.querySelector(`.loader${loaderNumber}`);
     if (animationElement) {
@@ -162,26 +166,27 @@ function deleteRecording(audioId, audioContainer) {
 }
 
 function stopRecording(buttonId) {
-    if (mediaRecorders[recordingIndex] && stateIndex === 1) {
-        mediaRecorders[recordingIndex].ondataavailable = (e) => {
-            chunks.push(e.data);
-        };
-
-        mediaRecorders[recordingIndex].onstop = () => {
-            const blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
-            audioURLs[recordingIndex] = window.URL.createObjectURL(blob);
-
-            // Add the new AudioBar after the audio URL is updated
-            addNewAudioBar(buttonId);
-            
-            // Clear the chunks array for the next recording
-            chunks = [];
-        };
-
-        mediaRecorders[recordingIndex].stop();
-    } else {
-        console.error('mediaRecorders[recordingIndex] is undefined');
-    }
+    // if (mediaRecorders[recordingIndex] && stateIndex === 1) {
+    //     mediaRecorders[recordingIndex].ondataavailable = (e) => {
+    //         chunks.push(e.data);
+    //     };
+    //
+    //     mediaRecorders[recordingIndex].onstop = () => {
+    //         const blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+    //         audioURLs[recordingIndex] = window.URL.createObjectURL(blob);
+    //
+    //         // Add the new AudioBar after the audio URL is updated
+    //         // addNewAudioBar(buttonId);
+    //
+    //         // Clear the chunks array for the next recording
+    //         chunks = [];
+    //     };
+    //
+    //     mediaRecorders[recordingIndex].stop();
+    // } else {
+    //     console.error('mediaRecorders[recordingIndex] is undefined');
+    // }
+    console.log('stopped recording')
     stateIndex = 0; // stopped recording
 }
 
