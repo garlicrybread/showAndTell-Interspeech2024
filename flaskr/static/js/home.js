981fde8aa3ed469080d3c8c7636ee1db3a95e23a
garlicrybread <garlicrybread@gmail.com>
2024-04-15 10:43:39 -0500

    added front page buttons and js to navigate to appropriate pages when pressed

--- flaskr/static/js/home.js
+++ flaskr/static/js/home.js
@@ -7,8 +7,22 @@ if (window.location.href.includes("/pronunciationVis/")) {
 }
 
 var button = document.getElementById('btnUserId')
+const calNavBtn  = document.getElementById('calibrationNavBtn')
+const tutNavBtn = document.getElementById('tutorialNavBtn')
+const pracNavBtn = document.getElementById('practiceNavBtn')
 
 button.addEventListener("click", saveUserId);
+calNavBtn.addEventListener("click", function() {
+    navigateToRoute('vowelCalibration');
+});
+tutNavBtn.addEventListener("click", function() {
+    navigateToRoute('tutorial/vowelP1');
+});
+pracNavBtn.addEventListener("click", function() {
+    navigateToRoute('practice/chart');
+});
+
+
 async function saveUserId() {
     // Retrieve the value of the input field with id "userId"
     const userId = document.getElementById("textInputId").value;
@@ -18,4 +32,11 @@ async function saveUserId() {
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(userId)
     })
+}
+
+async function navigateToRoute(location) {
+    // Retrieve the value of the input field with id "userId"
+    console.log(location)
+    const path = `${urlPath}${location}`
+    window.location.href = path
 }
