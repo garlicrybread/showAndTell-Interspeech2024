6b55565a277476e0ccbaf02342304fef7ac1ad82
garlicrybread <garlicrybread@gmail.com>
2024-04-16 09:43:57 -0500

    moved saveUserId into coordinates

--- flaskr/static/js/home.js
+++ flaskr/static/js/home.js
@@ -1,14 +1,11 @@
 import {navigateToRoute} from './myJS.js'
 import {urlPath} from './myJS.js'
 
-var button = document.getElementById('btnUserId')
 const calNavBtn  = document.getElementById('calibrationNavBtn')
 const tutNavBtn = document.getElementById('tutorialNavBtn')
 const pracNavBtn = document.getElementById('practiceNavBtn')
 
-button.addEventListener("click", saveUserId);
 calNavBtn.addEventListener("click", function() {
-    console.log('in cal nav btn')
     navigateToRoute('vowelCalibration');
 });
 tutNavBtn.addEventListener("click", function() {
@@ -19,14 +16,4 @@ pracNavBtn.addEventListener("click", function() {
 });
 
 
-async function saveUserId() {
-    // Retrieve the value of the input field with id "userId"
-    const userId = document.getElementById("textInputId").value;
-    const path = `${urlPath}api/saveUserId`
-    fetch(path, {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(userId)
-    })
-}
 
