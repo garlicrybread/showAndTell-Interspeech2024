c176c524a8b9fb34a76434d83514a35b43cb338a
garlicrybread <garlicrybread@gmail.com>
2024-04-16 09:45:30 -0500

    added home button to tutorial page

--- flaskr/static/js/vwlTutorial.js
+++ flaskr/static/js/vwlTutorial.js
@@ -1,5 +1,9 @@
-import {toggleText} from "./myJS.js";
+import {navigateToRoute, toggleText} from "./myJS.js";
 
 document.getElementById("recordWord").addEventListener("click", function () {
     toggleText("recordWord");
+});
+
+homeNavBtn.addEventListener('click', function () {
+    navigateToRoute('')
 });
