de785ace39432d29e9be744637376dc308d47b8c
garlicrybread <garlicrybread@gmail.com>
2024-08-01 14:04:26 -0500

    if in practice, get button ids

--- flaskr/static/js/practice.js
+++ flaskr/static/js/practice.js
@@ -1,43 +1,57 @@
  import {drawVowelChart, drawVowels} from "../../static/js/vwlChart.js";
  import {audioToJson, navigateToRoute, toggleText} from "./myJS.js";
 
- // home nav button
- const homeNavBtn = document.getElementById('homeNavBtn');
- homeNavBtn.addEventListener('click', function () {
-     navigateToRoute('')
- });
+ if (window.location.href.includes("/practice")) {
+     document.addEventListener('DOMContentLoaded', (event) => {
+         const tabName = 'pair1';
+         drawVowelChart(tabName); // Change 'pair1' to the ID of the first tab content
+         var spaPath = `/Users/hearth/PycharmProjects/showAndTell-SP24/flaskr/static/participantData/spaM0/${tabName}0/spaM0-${tabName}`;
+         var data = `{"gotAudio": "${spaPath}0-p0.wav"}`;
+         audioToJson(data,tabName,'NA',true);
+         spaPath = `/Users/hearth/PycharmProjects/showAndTell-SP24/flaskr/static/participantData/spaM0/${tabName}1/spaM0-${tabName}`;
+         data = `{"gotAudio": "${spaPath}1-p1.wav"}`;
+         audioToJson(data,tabName,'NA',true);
+     });
+     window.openTab = openTab
 
+     // home nav button
+     const homeNavBtn = document.getElementById('homeNavBtn');
+     homeNavBtn.addEventListener('click', function () {
+         navigateToRoute('')
+     });
 
- const p11btn = 'p11'
- const p12btn = 'p12'
- const p21btn = 'p21'
- const p22btn = 'p22'
- const p31btn = 'p31'
- const p32btn = 'p32'
- // define strings for svg ids
- const svgP1 = 'pair1'
- const svgP2 = 'pair2'
- const svgP3 = 'pair3'
- document.getElementById(p11btn).addEventListener("click", function () {
-     toggleText(p11btn, svgP1);
- });
- // document.getElementById(p12btn).addEventListener("click", function () {
- //     toggleText(p12btn, svgP1);
- // });
+     const p11btn = 'p11'
+     const p12btn = 'p12'
+     const p21btn = 'p21'
+     const p22btn = 'p22'
+     const p31btn = 'p31'
+     const p32btn = 'p32'
+     // define strings for svg ids
+     const svgP1 = 'pair1'
+     const svgP2 = 'pair2'
+     const svgP3 = 'pair3'
+     document.getElementById(p11btn).addEventListener("click", function () {
+         toggleText(p11btn, svgP1);
+     });
+     // document.getElementById(p12btn).addEventListener("click", function () {
+     //     toggleText(p12btn, svgP1);
+     // });
 
- document.getElementById(p21btn).addEventListener("click", function () {
-     toggleText(p21btn, svgP2);
- });
- // document.getElementById(p22btn).addEventListener("click", function () {
- //     toggleText(p22btn, svgP2);
- // });
+     document.getElementById(p21btn).addEventListener("click", function () {
+         toggleText(p21btn, svgP2);
+     });
+     // document.getElementById(p22btn).addEventListener("click", function () {
+     //     toggleText(p22btn, svgP2);
+     // });
 
- document.getElementById(p31btn).addEventListener("click", function () {
-     toggleText(p31btn, svgP3);
- });
+     document.getElementById(p31btn).addEventListener("click", function () {
+         toggleText(p31btn, svgP3);
+     });
+ }
  // document.getElementById(p32btn).addEventListener("click", function () {
  //     toggleText(p32btn, svgP3);
  // });
+
 
 // JavaScript function to open a tab
 export function openTab(evt, tabName) {
@@ -67,14 +81,3 @@ export function openTab(evt, tabName) {
 };
 
  // Optionally open the first tab by default on page load
- document.addEventListener('DOMContentLoaded', (event) => {
-     const tabName = 'pair1';
-     drawVowelChart(tabName); // Change 'pair1' to the ID of the first tab content
-     var spaPath = `/Users/hearth/PycharmProjects/showAndTell-SP24/flaskr/static/participantData/spaM0/${tabName}0/spaM0-${tabName}`;
-     var data = `{"gotAudio": "${spaPath}0-p0.wav"}`;
-     audioToJson(data,tabName,'NA',true);
-     spaPath = `/Users/hearth/PycharmProjects/showAndTell-SP24/flaskr/static/participantData/spaM0/${tabName}1/spaM0-${tabName}`;
-     data = `{"gotAudio": "${spaPath}1-p1.wav"}`;
-     audioToJson(data,tabName,'NA',true);
- });
-window.openTab = openTab
